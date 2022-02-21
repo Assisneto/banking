0d275de6a0ce6b50954c3bcfb12b57a4c7dbcc65
Assisneto Damasceno <netodamasceno12@gmail.com>
2022-02-21 17:46:46 -0300

    add create user endpoint

---
+++ lib/stone_banking_web/controllers/users_controller.ex
@@ -0,0 +1,23 @@
+defmodule StoneBankingWeb.UsersController do
+  use StoneBankingWeb, :controller
+  alias StoneBanking.Users
+  alias StoneBanking.Users.User
+
+  def create(conn, params),
+    do:
+      StoneBanking.Users.create_user(params)
+      |> format_user()
+      |> send_json(conn, 201)
+
+  def format_user({:ok, %User{} = user}),
+    do: %{
+      name: user.name,
+      email: user.email
+    }
+
+  def send_json(data, conn, status) do
+    conn
+    |> put_resp_header("content-type", "application/json")
+    |> send_resp(status, Jason.encode!(data))
+  end
+end
